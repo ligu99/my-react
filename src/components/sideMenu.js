5fcb06362cfd80d9caba5a90e1b4ad28476b653d
Phil.Lai <Phil.Lai@cn.ey.com>
2024-12-26 19:00:48 +0800

    提取菜单扁平化逻辑

--- src/components/sideMenu.js
+++ src/components/sideMenu.js
@@ -37,10 +37,12 @@ const menuList=[
 ]
 function SideMenu(props){
     const [highMenu, setHighMenu]=useState("");
+    const [allMenuList, setAllMenuList]=useState([]);
     const [openKeys, setOpenKeys]=useState("");
-    const [loading, setLoading]=useState(false);
-    const onClickMenu=(menuItem)=>{
-        console.log(menuItem);
+    const [loading, setLoading]=useState(true);
+
+    // 菜单扁平化
+    const getMenuList=()=>{
         let menuArr = [];
         menuList.forEach(x=>{
             if(x.children && x.children.length>0){
@@ -51,38 +53,44 @@ function SideMenu(props){
                 menuArr.push(x);
             }
         })
-        const currMenu=menuArr.filter(x=>x.key===menuItem.key)
+        return menuArr
+    }
+    // 点击菜单
+    const onClickMenu=(menuItem)=>{
+        console.log(menuItem);
+        const currMenu=allMenuList.filter(x=>x.key===menuItem.key)
         props.history.push({
             pathname:currMenu[0].path
         })
+        // 调用设置菜单高亮
         setHighLightMenu(currMenu[0].path)
     }
     // 设置菜单高亮
     const setHighLightMenu=(path="")=>{
         const pathname = path || props.location.pathname
-        let menuArr = [];
-        menuList.forEach(x=>{
-            if(x.children && x.children.length>0){
-                x.children.forEach(x=>{
-                    menuArr.push(x);
-                })
-            }else{
-                menuArr.push(x);
-            }
-        })
-        const currMenu=menuArr.filter(x=>x.path===pathname)
+        const currMenu=allMenuList.filter(x=>x.path===pathname)
         setHighMenu(currMenu[0].key)
+        // 设置展开菜单
         if(currMenu[0].prevkey){
             setOpenKeys(currMenu[0].prevkey);
         }
-        setLoading(true)
+        setLoading(false);
     }
+    // 加载组件就对菜单列表扁平化
     useEffect(()=>{
-        setHighLightMenu()
+        let resArr = getMenuList();
+        setAllMenuList(resArr);//setAllMenuList是异步的，所以在后面马上执行setHighLightMenu是没用的，allMenuList还是原来的空数组
     },[])
+
+    // 因为setAllMenuList是异步的，所以在这里依赖allMenuList，进行设置菜单高亮
+    useEffect(()=>{
+        if(allMenuList.length>0){
+            setHighLightMenu();
+        }
+    },[allMenuList])
     return (
         <Sider trigger={null} collapsible collapsed={props.collapsed}>
-            {loading && <Menu
+            {!loading && <Menu
             onClick={onClickMenu}
             defaultOpenKeys={[openKeys]}
             theme="dark"
